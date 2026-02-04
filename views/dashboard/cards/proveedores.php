8806251dee2226844dd1253525035d81e16336a8
Ikian <ikiansaa1@gmail.com>
2026-02-04 00:44:59 -0500

    Reinicio

--- views/dashboard/cards/proveedores.php
+++
@@ -1,5 +0,0 @@
-<div class="card">
-    <h3><?= htmlspecialchars($cardData['label']) ?></h3>
-    <p><strong><?= (int) $cardData['count'] ?></strong> registros</p>
-    <a class="btn" href="<?= htmlspecialchars($cardData['route']) ?>">Ir a módulo</a>
-</div>
